8f0878cc02aebcb047cf6b7e8593a4dff4bf0970
RAFART94 <danielgrs94@gmail.com>
2023-12-18 22:01:15 +0000

    Probando motor de Jinja

---
+++ app_ingresos_gastos/routes.py
@@ -0,0 +1,21 @@
+from app_ingresos_gastos import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    datos = [
+        {'fecha': '01/01/2023',
+        'concepto': 'Salario',
+        'monto': '1500'},
+        {'fecha': '05/01/2023',
+        'concepto': 'Ropa',
+        'monto': '-150'},
+        {'fecha': '17/01/2023',
+        'concepto': 'Supermercado',
+        'monto': '-250'}  
+    ]
+    return render_template('index.html', data = datos)
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
