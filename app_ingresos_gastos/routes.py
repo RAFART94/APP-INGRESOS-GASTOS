5af9b66d5792d55ff747d904411fbb128b79e91a
RAFART94 <danielgrs94@gmail.com>
2023-12-19 22:18:15 +0000

    Validación de fechas en metodo new()

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -1,35 +1,41 @@
 from app_ingresos_gastos import app
 from flask import render_template, request, redirect
 import csv
+from datetime import date
 
 @app.route("/")
 def index():
     datos = []
     #llamada al archivo csv
-    fichero = open('data/movimiento.csv', 'r')
+    fichero = open('data/movimientos.csv','r')
     #accediendo a cada registro de archivo y darle formato
     csvReader = csv.reader(fichero, delimiter=',', quotechar='"')
     for items in csvReader:
         datos.append(items)
 
-    return render_template('index.html', data = datos, titulo = 'Lista')
+    return render_template('index.html', data = datos, titulo= 'Lista')
 
-@app.route('/new', methods = ['GET', 'POST'])
+@app.route('/new', methods= ['GET', 'POST'])
 def new():
-    if request.method == 'POST':
+    if request.method== 'POST':
+        hoy = str(date.today())#Esto quita la fecha de hoy
+
+        if request.form['fecha'] > hoy:
+            return render_template('new.html', titulo = 'Nuevo', tipoAccion = 'Registro', tipoBoton = 'Guardar')
         #return 'esto debería registrase' + str(request.form)
 
-        #acceder al archivo y configurar para la carga de nuevo registro
-        mifichero = open('data/movimientos.csv', 'a', newline = '')
-        #llamar al metodo writer de escritura y configuramos formato
-        lectura = csv.writer(mifichero, delimiter=',', quotechar='"')
-        #registramos los datos recibidos en el archivo csv
-        lectura.writerow([request.form['fecha'], request.form['concepto'], request.form['moto']])
-        mifichero.close()
+        if request.form['fecha'] <= hoy:
+            #acceder al archivo y configurar para la carga de nuevo registro
+            mifichero = open('data/movimientos.csv', 'a', newline='')
+            #llamar al metodo writer de escritura y configuramos formato
+            lectura = csv.writer(mifichero, delimiter=',', quotechar='"')
+            #registramos los datos recibidos en el archivo csv
+            lectura.writerow([request.form['fecha'], request.form['concepto'], request.form['monto']])
+            mifichero.close()
 
-        return redirect('/')
+            return redirect('/')
     
-    else:
+    else:#Si es GET
         return render_template('new.html', titulo = 'Nuevo', tipoAccion = 'Registro', tipoBoton = 'Guardar')
 
 @app.route('/update')
--- pruebas.py
+++ pruebas.py
@@ -23,7 +23,7 @@ for items in lectura:
 print("mi lista:",midato[0][1])    
 """
 #ejemplo para registrar datos en csv
-
+'''
 import csv
 
 mifichero = open('data/movimientos.csv','a',newline='')
@@ -31,3 +31,9 @@ lectura = csv.writer(mifichero,delimiter=',', quotechar='"')
 lectura.writerow(['24/04/2024','roscon de reyes','-40'])
 
 mifichero.close()
+'''
+
+from datetime import date
+
+print(type(str(date.today())))
+print(date.today())
