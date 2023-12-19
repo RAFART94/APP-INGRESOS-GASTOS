b2d732539e48c0cbdade228f7a268c1e3f1cb66c
RAFART94 <danielgrs94@gmail.com>
2023-12-19 21:58:45 +0000

    Agregar funcion de guardado dentro de metodo new

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -1,29 +1,41 @@
 from app_ingresos_gastos import app
-from flask import render_template
+from flask import render_template, request, redirect
+import csv
 
 @app.route("/")
 def index():
-    datos = [
-        {'fecha': '01/01/2023',
-        'concepto': 'Salario',
-        'monto': '1500'},
-        {'fecha': '05/01/2023',
-        'concepto': 'Ropa',
-        'monto': '-150'},
-        {'fecha': '17/01/2023',
-        'concepto': 'Supermercado',
-        'monto': '-250'}  
-    ]
-    return render_template('index.html', data = datos)
+    datos = []
+    #llamada al archivo csv
+    fichero = open('data/movimiento.csv', 'r')
+    #accediendo a cada registro de archivo y darle formato
+    csvReader = csv.reader(fichero, delimiter=',', quotechar='"')
+    for items in csvReader:
+        datos.append(items)
 
-@app.route('/new')
+    return render_template('index.html', data = datos, titulo = 'Lista')
+
+@app.route('/new', methods = ['GET', 'POST'])
 def new():
-    return render_template('new.html')
+    if request.method == 'POST':
+        #return 'esto debería registrase' + str(request.form)
+
+        #acceder al archivo y configurar para la carga de nuevo registro
+        mifichero = open('data/movimientos.csv', 'a', newline = '')
+        #llamar al metodo writer de escritura y configuramos formato
+        lectura = csv.writer(mifichero, delimiter=',', quotechar='"')
+        #registramos los datos recibidos en el archivo csv
+        lectura.writerow([request.form['fecha'], request.form['concepto'], request.form['moto']])
+        mifichero.close()
+
+        return redirect('/')
+    
+    else:
+        return render_template('new.html', titulo = 'Nuevo', tipoAccion = 'Registro', tipoBoton = 'Guardar')
 
 @app.route('/update')
 def update():
-    return render_template('update.html')
+    return render_template('update.html', titulo = 'Actualizar', tipoAccion = 'Actualización', tipoBoton = 'Editar')
 
 @app.route('/delete')
 def delete():
-    return render_template('delete.html')
+    return render_template('delete.html', titulo = 'Borrar')
---
+++ pruebas.py
@@ -0,0 +1,33 @@
+# lectura de archivos
+'''
+with open('data/movimientos.csv','r') as resultado:
+    leer = resultado.read()
+    print(type(leer))
+'''
+#otro ejemplo de lectura de archivos csv
+"""
+resultado = open('data/movimientos.csv','r')
+lectura = resultado.readlines()
+print(lectura)   
+"""
+"""
+import csv
+
+midato= []
+mifichero = open('data/movimientos.csv','r')
+lectura = csv.reader(mifichero,delimiter=",",quotechar='"')
+for items in lectura:
+    #print(type(items))
+    midato.append(items)
+
+print("mi lista:",midato[0][1])    
+"""
+#ejemplo para registrar datos en csv
+
+import csv
+
+mifichero = open('data/movimientos.csv','a',newline='')
+lectura = csv.writer(mifichero,delimiter=',', quotechar='"')
+lectura.writerow(['24/04/2024','roscon de reyes','-40'])
+
+mifichero.close()
